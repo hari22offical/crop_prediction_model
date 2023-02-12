1ae1f223738d6e2db1fd7c7a189798fa2f75c4d1
Hari Narayanan R <117098032+hari22offical@users.noreply.github.com>
2023-02-12 13:44:13 +0530

    WEBAPP

--- ML 03/crop_streamlit.py
+++ ML 03/crop_streamlit.py
@@ -1,17 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb 12 11:31:11 2023
-
-@author: harin
-"""
-
 import numpy as np
+import pandas as pd
 import pickle
 import streamlit as st
-import pandas as pd
+from PIL import Image
 
 # Loading the trained model
-loaded_model = pickle.load(open("C:/Users/harin/Pictures/ML 03/crop_prediction.pkl","rb"))
+loaded_model = pickle.load(open("C:/Users/jeyasri/Downloads/CROP/crop_prediction.pkl","rb"))
 
 def crop_prediction(input_data):
     
@@ -25,23 +19,30 @@ def crop_prediction(input_data):
    
     
 def main():
+    image = Image.open("C:/Users/jeyasri/Downloads/CROP/crop_image.jpg")
+    st.image(image, width=700)
     
     st.title("Crop Prediction App")
-    
-    st.write(pd.DataFrame({
-    'state_name': ['Andaman and Nicobar','Tamil Nadu','Andhra Pradesh','Assam','Chattisgarh','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu and Kashmir','Karnataka','Kerala','Madhya Pradesh','Maharashtra','Manipur','Meghalaya','Nagaland','Odisha','Pondicherry','Punjab','Rajasthan','Tamil Nadu','Telangana','Tripura','Uttar Pradesh','Uttrakhand','West Bengal'],
-    'numeric values': [0,20,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
-    }))
 
     STATE = st.number_input("Enter the State:", min_value=0, max_value=25)
-     
-    N_SOIL = st.number_input("Enter the Nitrogen content of Soil:")
-    P_SOIL = st.number_input("Enter the Phosphorous content of Soil:")
-    K_SOIL = st.number_input("Enter the Pottasium content of Soil:")
-    ph = st.number_input("Enter the pH of Soil:", min_value=0, max_value=14)
-    TEMPERATURE = st.number_input("Enter the Temperature of the State:", min_value=0, max_value=75)
-    HUMIDITY = st.number_input("Enter the Relative humidity (%):", min_value=0, max_value=100)
-    RAINFALL = st.number_input("Enter the Average rainfall (in mm):", min_value=0, max_value=1000)
+    st.write(pd.DataFrame({
+   'STATE': ['Andaman and Nicobar','Tamil Nadu','Andhra Pradesh','Assam','Chattisgarh','Goa','Gujarat','Haryana','Himachal Pradesh','Jammu and Kashmir','Karnataka','Kerala','Madhya Pradesh','Maharashtra','Manipur','Meghalaya','Nagaland','Odisha','Pondicherry','Punjab','Rajasthan','Tamil Nadu','Telangana','Tripura','Uttar Pradesh','Uttrakhand','West Bengal'],
+   'EQUIVALENT VALUE': [0,20,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
+   }))
+    N_SOIL = st.slider('Nitrogen content of Soil', 0, 150)
+    st.write(N_SOIL)
+    P_SOIL = st.slider('Phosphorous content of Soil', 0, 150)
+    st.write(P_SOIL)
+    K_SOIL = st.slider('Pottasium content of Soil', 0, 150)
+    st.write(K_SOIL)
+    ph = st.slider('pH of Soil', 0, 14)
+    st.write(ph)
+    TEMPERATURE = st.slider('Temperature of the State', 0, 75)
+    st.write(TEMPERATURE)
+    HUMIDITY = st.slider('Relative humidity (%)', 0, 100)
+    st.write(HUMIDITY)
+    RAINFALL = st.slider('Enter the Average rainfall (in mm)', 0, 1000)
+    st.write(RAINFALL)
     CROP_PRICE = st.number_input("Enter the Price of the crop:", min_value=0)
     
     # Prediction code
